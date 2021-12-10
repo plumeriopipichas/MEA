50fbafcb071f4f2bd78f21497a0a32efadbd7221
plumeriopipichas <plumeriopipichas@gmail.com>
2021-12-10 16:07:49 -0600

    Add files via upload

--- funciones_para_MEA.R
+++ funciones_para_MEA.R
@@ -19,14 +19,17 @@ arma_base<-function(video,cut_above = 0,cut_below = 0){
 }
 
 #-----------------------------------------------------
-#funcion partition_data para separar los datos del video en bloques de la longitud de tiempo dada
+#   funcion partition_data para separar los datos del video en bloques de la longitud de tiempo dada
+#   pasos debe ser entero positivo, de preferencia divisor del largo de los bloques
 
-partition_data <- function(datos,minutos=5,sr=30){
+partition_data <- function(datos,minutos=5,sr=30,pasos=1){
   partes <- list()
   largo_bloques <- minutos*sr*60
-  num_partes <- floor(nrow(datos)/largo_bloques)
+  num_partes <- pasos*floor(nrow(datos)/largo_bloques)
   for (i in 1:num_partes){
-    partes[[i]] <- datos[((i-1)*largo_bloques+1):(i*largo_bloques), ]
+        inicia <- (i-1)*floor(largo_bloques/pasos)+1
+        termina <- inicia + largo_bloques -1
+        partes[[i]] <- datos[inicia:termina, ]
   }
   return(partes)
 }
@@ -45,10 +48,22 @@ suavizar <- function(listado,n=7){
   return(aux)
 }
 
+#----------------- funcion para detectar lapsos con valores altos. Dada una lista en que cada entrada es
+# un vector de datos numericos, devuelve los indices que superan el cuantil q dado-------------
 
-#-----------
+detecta_altos <- function(lista_datos,q=0.5){
+      promedios <- numeric()
+      for (i in 1:length(lista_datos)){
+            promedios <- c(promedios,mean(lista_datos[[i]],na.rm=TRUE)) 
+      }
+      altos <- which(promedios>quantile(promedios,probs=q))
+      return(altos)
+}
+
+
+#------------------------------
 # dado una particion de uno de los videos, selecciona los que sean relevantes por tener una correlacion cruzada alta, 
-# ya sea en promedio o en maximo. Devuelve lista con informacion de partes seleccionadas. EN CONSTRUCCION. 
+# ya sea en promedio o en maximo. Devuelve lista con informacion de partes seleccionadas. 
 
 
 crear_lista <- function(video_partido, nombre=NA, sr=30, segundos = 300, 
@@ -95,6 +110,23 @@ crear_lista <- function(video_partido, nombre=NA, sr=30, segundos = 300,
     return(selectos)
 } 
 
+#-------------------Crear la lista de periodos de medio minuto en que, para la roi que se indique, 
+#   hay mucho movimiento, tanto del paciente como del terapeuta *donde mucho es que sea mayor al cuantil dado*     
+
+crea_lista_movs <- function(video,q=0.85){
+          for (i in zonas){
+              temp <- video[ ,grep(i,names(video))]
+              temp1 <-as.data.frame(temp[ ,grep("paciente",names(temp))])
+              temp2 <- as.data.frame(temp[ ,grep("terapeuta",names(temp))])
+              print(dim(temp1))
+              altos1 <- detecta_altos(partition_data(temp1,minutos=0.5,pasos = 2,sr=30),q=q)
+              altos2 <- detecta_altos(partition_data(temp2,minutos=0.5,pasos = 2,sr=30),q=q)
+              ambos <- intersect(altos1,altos2)
+              print(i)
+              print(ambos)
+          }
+}
+        
 
 #--------------------FUNCIONES PARA GENERAR GRAFICAS A PARTIR DE LA LISTA DE SELECCIONADOS -----------------
 
@@ -102,9 +134,9 @@ corta_lista <- function(id){
       x <- which(lista_seleccionados$id==id)
       aux <- partition_data(completa[[ lista_seleccionados$video[x] ]])[[ lista_seleccionados$num_periodo[x] ]]
       aux <- aux[ ,grep(lista_seleccionados$zona[x],names(aux))]
-      aux$suave_paciente <- suavizar(aux[ ,1],19)
-      aux$suave_terapeuta <- suavizar(aux[ ,2],19)
-      aux$tiempo <- (1:dim(aux)[1]/(60*19))+lista_seleccionados$minuto_inicio[x]
+      aux$suave_paciente <- suavizar(aux[ ,1],30)
+      aux$suave_terapeuta <- suavizar(aux[ ,2],30)
+      aux$tiempo <- (1:dim(aux)[1]/(60*30))+lista_seleccionados$minuto_inicio[x]
       temp <- list()
       temp[[1]]<-aux
       temp[["video"]]<-paste("Video: ",lista_seleccionados$video[x])
