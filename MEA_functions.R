ef7ac3766afe5a0b2ad70e4090bcaf54eac87eec
plumeriopipichas <plumeriopipichas@gmail.com>
2023-03-15 15:06:16 -0600

    Add files via upload

--- MEA_functions.R
+++ MEA_functions.R
@@ -14,8 +14,141 @@ arma_base<-function(video,cut_above = 0,cut_below = 0,nombres = rois){
   ca <- cut_above + 1
   cb <- dim(x)[1]-cut_below
   x <- x[ca:cb, ]
-  names(x)<-c("tiempo",nombres)
+  names(x)<-c("time",nombres)
   return(x)
+}
+
+
+#-------------------Crear la lista de periods de medio minute en que, para la roi que se indique, 
+#   hay mucho movimiento, tanto del patient como del therapist *donde mucho es que sea mayor al cuantil dado*     
+
+crea_lista_movs <- function(vp,q=0.71,nombre_video=NA,sr=30,lagmax=150){
+  selectos <-data.frame(matrix(ncol = 15,nrow = 0))  
+  colnames(selectos) <- c("video","zone","num_period","start","end",
+                          "mov_patient","mov_therapist","mov_medio",
+                          "spearman_max","lag_spearman_max",
+                          "spearman_min","lag_spearman_min",
+                          "leads","sex","type")
+  
+  for (k in zones){
+    temp<-list()
+    temp1<-list()
+    temp2<-list()
+    for (i in 1:length(vp)){
+      temp[[i]] <- vp[[i]][ ,grep(k,names(vp[[i]]))]
+      temp1[[i]] <- suavizar(
+        as.numeric(temp[[i]][ ,grep("patient",names(temp[[i]]))]),30)
+      temp2[[i]] <- suavizar(
+        as.numeric(temp[[i]][ ,grep("therapist",names(temp[[i]]))]),30)
+    }
+    aux1 <- detecta_altos(temp1,q)
+    aux2 <- detecta_altos(temp2,q)
+    Aux<-intersect(aux1,aux2)
+    for (j in Aux){
+      renglon <- data.frame(matrix(ncol=ncol(selectos),nrow=1))
+      names(renglon)<-names(selectos)
+      renglon$video <- nombre_video
+      renglon$zone <- k
+      renglon$num_period <- j
+      renglon$start <- round(vp[[j]]$time[1]/(60*sr),1)
+      renglon$end <- round(vp[[j]]$time[nrow(vp[[j]])]/(60*sr),1)
+      renglon$mov_patient <- mean(temp1[[j]])
+      renglon$mov_therapist <- mean(temp2[[j]])
+      renglon<-mutate(renglon,mov_medio=mean(c(mov_patient,mov_therapist)))
+      cecefe_s <- ccf(rank(temp1[[j]]),rank(temp2[[j]]),lag.max = lagmax,plot=FALSE)
+      y<-cecefe_s$acf[ ,1,1]
+      x1<-which(y==max(y))
+      x2<-which(y==min(y))
+      renglon$spearman_max <- y[x1]
+      renglon$spearman_min <- y[x2]
+      renglon$lag_spearman_max <- round(cecefe_s$lag[x1,1,1]/sr,2)
+      renglon$lag_spearman_min <- round(cecefe_s$lag[x2,1,1]/sr,2)
+      renglon$leads<-"-"
+      signo <- renglon$spearman_max-abs(renglon$spearman_min)    
+      if (signo>0 & renglon$lag_spearman_max>0.25){
+        renglon$leads="therapist"
+      }
+      if (signo>0 & renglon$lag_spearman_max< -0.25 ){
+        renglon$leads="patient"
+      }
+      if (signo<0 & renglon$lag_spearman_min>0.25){
+        renglon$leads="therapist"
+      }
+      if (signo<0 & renglon$lag_spearman_min< -0.25 ){
+        renglon$leads="patient"
+      }
+      x <- which(logbook$file == renglon$video)
+      renglon$sex <- logbook$sex[x]
+      renglon$type <- logbook$classification[x]
+      selectos<-rbind(selectos,renglon)
+    }
+  }          
+  return(selectos)
+}        
+
+
+#------------------------------
+# dado una particion de uno de los videos, selecciona los que sean relevantes por tener una correlacion 
+#cruzada alta,  ya sea en promedio o en maximo. Devuelve lista con informacion de partes seleccionadas. 
+
+crear_lista <- function(video_partido, nombre=NA, sr=30, segundos = 300, 
+                        umbral_max=0.7,umbral_min=-0.7,umbral_mean=0.35,
+                        lag_mayor = 150){
+  selectos <- data.frame(matrix(ncol = 11,nrow = 0))  
+  colnames(selectos) <- c("video","zone","num_period","start","end",
+                          #"acf_max","acf_promedio","lag_acf_max",
+                          "spearman_max","lag_spearman_max",
+                          "spearman_min","lag_spearman_min","sex","type")
+  
+  for (i in 1:length(video_partido)){
+    for (j in zones){
+      temp <- video_partido[[i]][ ,grep(j,names(video_partido[[i]]))]
+      suave<-data.frame(suavizar(temp[ ,1],30),suavizar(temp[ ,2],30))
+      names(suave)<-names(temp)
+      temp<-suave
+      if(sum(abs(temp[1]))>1000 && sum(abs(temp[2]))>1000){
+        cecefe_s <- ccf(rank(temp[1]),rank(temp[2]),plot = FALSE,lag.max = 150,method="spearman")  
+        if (max(abs(cecefe_s$acf)>umbral_max) | abs(mean(cecefe_s$acf))>umbral_mean){
+          cecefe_s<-ccf(rank(temp[1]),rank(temp[2]),lag.max = lag_mayor,
+                        plot=FALSE,method="spearman")
+          y<-cecefe_s$acf[ ,1,1]
+          x1<-which(y==max(y))
+          x2<-which(y==min(y))
+          aux <- data.frame(matrix(ncol=ncol(selectos),nrow=1))
+          names(aux)<-names(selectos)
+          aux$video = nombre
+          aux$zone = j
+          aux$num_period <- i
+          aux$start <- round(video_partido[[i]]$tiempo[1]/(sr*60),1)
+          aux$end <- aux$start + round(segundos/60,2)
+          aux$spearman_max <- max(cecefe_s$acf)
+          aux$lag_spearman_max <- round(cecefe_s$lag[x1,1,1]/30,2)
+          aux$spearman_min <- min(cecefe_s$acf)
+          aux$lag_spearman_min <- round(cecefe_s$lag[x2,1,1]/30,2)
+          x <- which(logbook$file == aux$video)
+          aux$sex <- logbook$sex[x]
+          aux$type <- logbook$classification[x]
+          selectos<-rbind(selectos,aux)
+        }              
+      }
+      else{
+      }
+    } 
+  }
+  return(selectos)
+} 
+
+
+#----------------- funcion para detectar lapsos con valores altos. Dada una lista en que cada entrada es
+# un vector de datos numericos, devuelve los indices que superan el cuantil q dado-------------
+
+detecta_altos <- function(lista_datos,q=0.9){
+  promedios <- numeric()
+  for (i in 1:length(lista_datos)){
+    promedios <- c(promedios,mean(lista_datos[[i]],na.rm=TRUE)) 
+  }
+  altos <- which(promedios>quantile(promedios,probs=q))
+  return(altos)
 }
 
 #-----------------------------------------------------
@@ -48,136 +181,10 @@ suavizar <- function(listado,n=7){
   return(aux)
 }
 
-#----------------- funcion para detectar lapsos con valores altos. Dada una lista en que cada entrada es
-# un vector de datos numericos, devuelve los indices que superan el cuantil q dado-------------
-
-detecta_altos <- function(lista_datos,q=0.9){
-  promedios <- numeric()
-  for (i in 1:length(lista_datos)){
-    promedios <- c(promedios,mean(lista_datos[[i]],na.rm=TRUE)) 
-  }
-  altos <- which(promedios>quantile(promedios,probs=q))
-  return(altos)
-}
 
 
-#------------------------------
-# dado una particion de uno de los videos, selecciona los que sean relevantes por tener una correlacion 
-#cruzada alta,  ya sea en promedio o en maximo. Devuelve lista con informacion de partes seleccionadas. 
 
-crear_lista <- function(video_partido, nombre=NA, sr=30, segundos = 300, 
-                        umbral_max=0.7,umbral_min=-0.7,umbral_mean=0.35,lag_mayor = 90){
-  selectos <- data.frame(matrix(ncol = 11,nrow = 0))  
-  colnames(selectos) <- c("video","zone","num_period","start","end",
-                          #"acf_max","acf_promedio","lag_acf_max",
-                          "spearman_max","lag_spearman_max",
-                          "spearman_min","lag_spearman_min","sex","type")
-  
-  for (i in 1:length(video_partido)){
-    for (j in zones){
-      temp <- video_partido[[i]][ ,grep(j,names(video_partido[[i]]))]
-      suave<-data.frame(suavizar(temp[ ,1],30),suavizar(temp[ ,2],30))
-      names(suave)<-names(temp)
-      temp<-suave
-      if(sum(abs(temp[1]))>1000 && sum(abs(temp[2]))>1000){
-        cecefe_s <- ccf(rank(temp[1]),rank(temp[2]),plot = FALSE,lag.max = 150,method="spearman")  
-        if (max(abs(cecefe_s$acf)>umbral_max) | abs(mean(cecefe_s$acf))>umbral_mean){
-          cecefe_s<-ccf(rank(temp[1]),rank(temp[2]),lag.max = 150,plot=FALSE,method="spearman")
-          y<-cecefe_s$acf[ ,1,1]
-          x1<-which(y==max(y))
-          x2<-which(y==min(y))
-          aux <- data.frame(matrix(ncol=ncol(selectos),nrow=1))
-          names(aux)<-names(selectos)
-          aux$video = nombre
-          aux$zone = j
-          aux$num_period <- i
-          aux$start <- round(video_partido[[i]]$tiempo[1]/(sr*60),1)
-          aux$end <- aux$start + round(segundos/60,2)
-          aux$spearman_max <- max(cecefe_s$acf)
-          aux$lag_spearman_max <- round(cecefe_s$lag[x1,1,1]/30,2)
-          aux$spearman_min <- min(cecefe_s$acf)
-          aux$lag_spearman_min <- round(cecefe_s$lag[x2,1,1]/30,2)
-          x <- which(logbook$file == aux$video)
-          aux$sex <- logbook$sex[x]
-          aux$type <- logbook$classification[x]
-          selectos<-rbind(selectos,aux)
-        }              
-      }
-      else{
-      }
-    } 
-  }
-  return(selectos)
-} 
 
-#-------------------Crear la lista de periods de medio minute en que, para la roi que se indique, 
-#   hay mucho movimiento, tanto del patient como del therapist *donde mucho es que sea mayor al cuantil dado*     
-
-crea_lista_movs <- function(vp,q=0.71,nombre_video=NA,sr=30){
-  selectos <-data.frame(matrix(ncol = 15,nrow = 0))  
-  colnames(selectos) <- c("video","zone","num_period","start","end",
-                          "mov_patient","mov_therapist","mov_medio",
-                          "spearman_max","lag_spearman_max",
-                          "spearman_min","lag_spearman_min",
-                          "lidera","sex","type")
-  
-  for (k in zones){
-    temp<-list()
-    temp1<-list()
-    temp2<-list()
-    for (i in 1:length(vp)){
-      temp[[i]] <- vp[[i]][ ,grep(k,names(vp[[i]]))]
-      temp1[[i]] <- suavizar(
-        as.numeric(temp[[i]][ ,grep("patient",names(temp[[i]]))]),30)
-      temp2[[i]] <- suavizar(
-        as.numeric(temp[[i]][ ,grep("therapist",names(temp[[i]]))]),30)
-    }
-    aux1 <- detecta_altos(temp1,q)
-    aux2 <- detecta_altos(temp2,q)
-    Aux<-intersect(aux1,aux2)
-    for (j in Aux){
-      #print(c(j,"j","zone",k))
-      renglon <- data.frame(matrix(ncol=ncol(selectos),nrow=1))
-      names(renglon)<-names(selectos)
-      renglon$video <- nombre_video
-      renglon$zone <- k
-      renglon$num_period <- j
-      renglon$start <- round(vp[[j]]$tiempo[1]/(60*sr),1)
-      renglon$end <- round(vp[[j]]$tiempo[nrow(vp[[j]])]/(60*sr),1)
-      renglon$mov_patient <- mean(temp1[[j]])
-      renglon$mov_therapist <- mean(temp2[[j]])
-      renglon<-mutate(renglon,mov_medio=mean(c(mov_patient,mov_therapist)))
-      cecefe_s <- ccf(rank(temp1[[j]]),rank(temp2[[j]]),lag.max = 150,plot=FALSE)
-      #print(c("maximo",max(cecefe$acf),"promedio",mean(cecefe$acf)))
-      y<-cecefe_s$acf[ ,1,1]
-      x1<-which(y==max(y))
-      x2<-which(y==min(y))
-      renglon$spearman_max <- y[x1]
-      renglon$spearman_min <- y[x2]
-      renglon$lag_spearman_max <- round(cecefe_s$lag[x1,1,1]/sr,2)
-      renglon$lag_spearman_min <- round(cecefe_s$lag[x2,1,1]/sr,2)
-      renglon$lidera<-"-"
-      signo <- renglon$spearman_max-abs(renglon$spearman_min)    
-      if (signo>0 & renglon$lag_spearman_max>0.1 ){
-        renglon$lidera="therapist"
-      }
-      if (signo>0 & renglon$lag_spearman_max< -0.1 ){
-        renglon$lidera="patient"
-      }
-      if (signo<0 & renglon$lag_spearman_min>0.1 ){
-        renglon$lidera="therapist"
-      }
-      if (signo<0 & renglon$lag_spearman_min< -0.1 ){
-        renglon$lidera="patient"
-      }
-      x <- which(logbook$file == renglon$video)
-      renglon$sex <- logbook$sex[x]
-      renglon$type <- logbook$classification[x]
-      selectos<-rbind(selectos,renglon)
-    }
-  }          
-  return(selectos)
-}        
 
 #--------------------FUNCIONES PARA GENERAR GRAFICAS A PARTIR DE LA LISTA DE SELECCIONADOS -----------------
 
